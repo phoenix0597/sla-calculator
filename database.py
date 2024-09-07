9d35fec4364a79cfe078809ff97c78d30eb034a5
phoenix <phoenix0597@yandex.ru>
2024-09-07 19:46:57 +0300

    requirements.txt file added

--- database.py
+++ database.py
@@ -4,3 +4,10 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from config import settings
 
 engine = create_async_engine(settings.DB_URL, echo=True)
+async_session_maker = sessionmaker(bind=engine, class_=AsyncSession, autoflush=False, autocommit=False)
+Base = declarative_base()
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
